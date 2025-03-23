e31f714cda14b34047eb9aefb89a1936e3439149
reimyricardo <43665396+reimyricardo@users.noreply.github.com>
2025-03-23 12:53:28 -0400

    cambios

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if (currentHour < 12) {
+    greeting = "Buenos días Madelin"
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes Madelin"
+  } else {
+    greeting = "Buenas noches Madelin"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
